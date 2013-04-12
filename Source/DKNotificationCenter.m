d88aa9f5d747ab3ce8f62fc57ae31eac6fe5a548
Niels Grewe <thebeing+gna@halbordnung.de>
2013-04-12 21:57:13 +0000

    More build fixes.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/dbuskit/branches/0.1@36514 72102866-910b-0410-8b05-ffd578937521

--- Source/DKNotificationCenter.m
+++ Source/DKNotificationCenter.m
@@ -540,7 +540,7 @@
 
 - (NSUInteger)hash
 {
-  return ((NSUInteger)(uintptr_t)observer ^ (uintptr_t)selector);
+  return ((NSUInteger)(uintptr_t)observer ^ [NSStringFromSelector(selector) hash]);
 }
 
 - (BOOL)isEqual: (DKObservation*)other
